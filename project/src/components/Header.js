514f8008d803b14a9b20d0f78432ba01011df749
Yumin <dorayuminlee@gmail.com>
2022-01-13 20:21:19 +0900

    [feat] header 추가

---
+++ project/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <>
+            <div className="title">
+                <h1>YouTube Algorithm Breaker</h1>
+                <div id="emoji">😜</div>
+            </div>
+        </>
+    );
+}
+
+export default Header;
