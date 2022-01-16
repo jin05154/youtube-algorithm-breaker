9ca7e9d0cb3e645c22bbf41a4f1c62c372dd63f4
Yumin <dorayuminlee@gmail.com>
2022-01-17 04:25:46 +0900

    [feat] footer에 이메일 보내기 기능 추가

--- project/src/components/Footer.js
+++ project/src/components/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
     return (
         <div className="footer">
             <p>피드백은 언제나 환영입니다!</p>
-            <div id="email">
+            <div className="email">
                 <p>건의사항이 있으시다면&nbsp;</p>
-                <p>이메일</p>
-                <p>로 연락주세요 :)</p>
+                <p><a href="mailto: dorayuminlee@gmail.com">이메일</a></p>
+                <p>로 편하게 연락주세요 :)</p>
             </div>
             <div id="copyright">
                 <p>DESIGNED & CREATED BY Yumin Lee</p>
