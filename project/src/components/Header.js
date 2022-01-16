130ab3f1a108c75c414dc9501b130cce19fddc1b
Yumin <dorayuminlee@gmail.com>
2022-01-16 23:07:27 +0900

    [feat] header - 상세정보 추가

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -6,7 +6,11 @@ function Header() {
         <>
             <div className="title">
                 <h1>YouTube Algorithm Breaker</h1>
-                <div id="emoji">😜</div>
+                <div className="detail">
+                    <p>유튜브 추천영상, 더 이상 재미가 없나요?</p>
+                    <p>새로운 재미가 필요한가요?</p>
+                    <p>그렇다면 OP가 추천해드리는 채널의 영상들을 즐겨보세요! 🤩</p>
+                </div>
             </div>
         </>
     );
