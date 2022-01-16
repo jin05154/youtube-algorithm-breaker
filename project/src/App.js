0adbe83d634f131dab3d6240dcbf46789d9a88da
Yumin <dorayuminlee@gmail.com>
2022-01-16 23:23:12 +0900

    [feat] 이스터에그 추가

--- project/src/App.js
+++ project/src/App.js
@@ -2,10 +2,34 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
+  console.log(`
+
+
+  .∧,,∧
+  (；ω；) 하얗게
+  .∪　∪
+  し‐Ｊ
+  
+  ∧ ,';,';,',.
+  ( ; ω,';,',. 불태웠어..
+  .∪　∪
+  し‐Ｊ
+  
+  　　,';,',.
+  　';',,'...
+  .∪';',,'...
+  し‐Ｊ
+  
+  　　';,';,',.
+  　,';,',',..
+  ';',,'...
+  
+  
+  `);
   return (
-      <div className="App">
-          <Header />
-      </div>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
