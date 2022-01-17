3d33928f00a05fa02dde2c3f0621b23835d0fe57
Yumin <dorayuminlee@gmail.com>
2022-01-18 03:29:28 +0900

    [style] 색상코드 variable로 저장

--- project/src/components/MainPage.js
+++ project/src/components/MainPage.js
@@ -4,14 +4,14 @@ import './App.css';
 function Body() {
     return (
         <div className='container'>
-            <div id='page1' class='page'>page1</div>
-            <div id='page2' class='page'>
+            <div id='page1' class='page'>
                 <div className='detail'>
                     <p>유튜브 추천영상, 더 이상 재미가 없나요?</p>
                     <p>색다르고 새로운 재미가 필요한가요?</p>
                     <p>그렇다면 YAB이 추천해드리는 채널의 영상들을 즐겨보세요! 😎</p>
                 </div>
             </div>
+            <div id='page2' class='page'>page2</div>
             <div id='page3' class='page'>page3</div>
         </div>
     );
