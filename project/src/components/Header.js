92917a67bfc300051060e1649a088a889a4cb816
Yumin <dorayuminlee@gmail.com>
2022-01-17 15:10:26 +0900

    [style] code formatting

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -4,12 +4,19 @@ import './Header.css';
 function Header() {
     return (
         <>
-            <div className="title">
-                <h1>YouTube Algorithm Breaker</h1>
-                <div className="detail">
+            <div className='header'>
+                <div className='title'>
+                    <h1>Y</h1>
+                    <h1>ouTube&nbsp;</h1>
+                    <h1>A</h1>
+                    <h1>lgorithm&nbsp;</h1>
+                    <h1>B</h1>
+                    <h1>reaker</h1>
+                </div>
+                <div className='detail'>
                     <p>유튜브 추천영상, 더 이상 재미가 없나요?</p>
                     <p>새로운 재미가 필요한가요?</p>
-                    <p>그렇다면 YAB이 추천해드리는 채널의 영상들을 즐겨보세요! 🤩</p>
+                    <p>그렇다면 YAB이 추천해드리는 채널의 영상들을 즐겨보세요! 😎</p>
                 </div>
             </div>
         </>
