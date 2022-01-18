6e05f7af2128d5d7010e3f8b183552d5655b623d
Yumin <dorayuminlee@gmail.com>
2022-01-18 21:38:55 +0900

    [style] code formatting

--- project/src/components/Footer.js
+++ project/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css'
 
-function Footer() {
+const Footer = () => {
     return (
         <div className='footer'>
             <p>피드백은 언제나 환영입니다!</p>
--- project/src/components/MainPage.js
+++ project/src/components/MainPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './style.css';
 
-function Body() {
+const MainPage = () => {
     return (
         <div className='container'>
             <div id='page1' className='page'>
                 <div className='detail'>
                     <p>유튜브 추천영상, 더 이상 재미가 없나요?</p>
-                    <p>색다르고 새로운 재미가 필요한가요?</p>
-                    <p>그렇다면 YAB이 추천해드리는 채널의 영상들을 즐겨보세요! 😎</p>
+                    <p>볼게 없어서 맨날 무한도전 클립만 돌려보고 계신가요?</p>
+                    <p>그렇다면 YAB이 직접 엄선하여 추천해드리는 채널의 영상들을 즐겨보세요! 😎</p>
                 </div>
             </div>
             <div id='page2' className='page'>page2</div>
@@ -16,4 +16,4 @@ function Body() {
     );
 }
 
-export default Body;
+export default MainPage;
