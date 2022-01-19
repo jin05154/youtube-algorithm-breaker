1fdfa4c348854a97644c1275ff3c8fdd623d3060
Yumin <dorayuminlee@gmail.com>
2022-01-19 23:17:21 +0900

    [feat] 메인페이지 animated 말풍선 intro 추가

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-function Header() {
+import { ReactComponent as SendOne } from './img/send_1.svg';
+import { ReactComponent as SendTwo } from './img/send_2.svg';
+import { ReactComponent as SendThree } from './img/send_3.svg';
+import { ReactComponent as ReplyOne } from './img/reply_1.svg';
+
+const Chats = () => {
+    const [showOne, setShowOne] = useState(false);
+    const [showTwo, setShowTwo] = useState(false);
+    const [showThree, setShowThree] = useState(false);
+    const [showFour, setShowFour] = useState(false);
+    useEffect(() => {
+        setTimeout(function () {
+            setShowOne(true)
+        }, 500);
+        setTimeout(function () {
+            setShowTwo(true)
+        }, 1400);
+        setTimeout(function () {
+            setShowThree(true)
+        }, 2200);
+        setTimeout(function () {
+            setShowFour(true)
+        }, 3000);
+    }, [])
+    return (
+        <>
+            <div id='bubble-1'>{showOne && <SendOne />}</div>
+            <div id='bubble-2'>{showTwo && <SendTwo />}</div>
+            <div id='bubble-3'>{showThree && <ReplyOne />}</div>
+            <div id='bubble-4'>{showFour && <SendThree />}</div>
+        </>
+    );
+}
+
+const Header = () => {
     return (
         <>
             <div className='header'>
@@ -12,6 +46,9 @@ function Header() {
                     <h1>lgorithm&nbsp;</h1>
                     <h1>B</h1>
                     <h1>reaker</h1>
+                </div>
+                <div className='chat'>
+                    <Chats />
                 </div>
             </div>
         </>
