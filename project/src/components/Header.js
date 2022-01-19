178aff435b23fe947a3d73be28eb069537355654
Yumin <dorayuminlee@gmail.com>
2022-01-20 01:05:10 +0900

    [style] 기존 말풍선 애니메이션 구현 함수 삭제

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './style.css';
 
 import FadeIn from 'react-fade-in';
@@ -6,37 +6,6 @@ import { ReactComponent as SendOne } from './img/send_1.svg';
 import { ReactComponent as SendTwo } from './img/send_2.svg';
 import { ReactComponent as SendThree } from './img/send_3.svg';
 import { ReactComponent as ReplyOne } from './img/reply_1.svg';
-
-const Chats = () => {
-    const [showOne, setShowOne] = useState(false);
-    const [showTwo, setShowTwo] = useState(false);
-    const [showThree, setShowThree] = useState(false);
-    const [showFour, setShowFour] = useState(false);
-    useEffect(() => {
-        setTimeout(function () {
-            setShowOne(true)
-        }, 500);
-        setTimeout(function () {
-            setShowTwo(true)
-        }, 1400);
-        setTimeout(function () {
-            setShowThree(true)
-        }, 2200);
-        setTimeout(function () {
-            setShowFour(true)
-        }, 3000);
-    }, [])
-    return (
-        <>
-            <FadeIn>
-                <div id='bubble-1'><SendOne /></div>
-                <div id='bubble-2'><SendTwo /></div>
-                <div id='bubble-3'><ReplyOne /></div>
-                <div id='bubble-4'><SendThree /></div>
-            </FadeIn>
-        </>
-    );
-}
 
 const Header = () => {
     return (
