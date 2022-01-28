12a90e719ef474cc2d93a3a7550ef35f8d12b73f
Yumin <dorayuminlee@gmail.com>
2022-01-28 22:14:03 +0900

    [chore] pc, 모바일 화면용 svg 로고 추가

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
+import { ReactComponent as Title } from "../images/title.svg";
+import { ReactComponent as TitleM } from "../images/title_mobile.svg";
+
 const Header = () => {
     return (
-        <>
-            <div className='header'>
-                <div className='title'>
-                    <h1>Y</h1>
-                    <h1>ouTube&nbsp;</h1>
-                    <h1>A</h1>
-                    <h1>lgorithm&nbsp;</h1>
-                    <h1>B</h1>
-                    <h1>reaker</h1>
-                </div>
-            </div>
-        </>
+        <div className='header'>
+            <div className='pc-header'><Title /></div>
+            <div className='mobile-header'><TitleM /></div>
+        </div>
     );
 }
 
