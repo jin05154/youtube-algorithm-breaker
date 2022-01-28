b4fd3c717183be1fc42a2bb0649d2b3df6b73380
Yumin <dorayuminlee@gmail.com>
2022-01-28 22:14:43 +0900

    [style] 모바일 화면용 미디어 쿼리 추가

--- project/src/routes/HomePage.js
+++ project/src/routes/HomePage.js
@@ -19,8 +19,11 @@ export default function HomePage() {
         <div className="detail">
           <p>유튜브 추천영상, 더 이상 재미가 없나요?</p>
           <p>볼게 없어서 맨날 무한도전 클립만 돌려보고 계신가요?</p>
-          <p>그렇다면 YAB이 직접 엄선하여 추천해드리는 영상들을 즐겨보세요!</p>
-          <div>
+          <div id="last-detail">
+            <p>그렇다면 TAB이 직접 엄선하여&nbsp;</p>
+            <p>추천해드리는 영상들을 즐겨보세요!</p>
+          </div>
+          <div id="main-btn">
             <Button text={"추천 받아보기 👋"} onClick={onClickMainBtn} />
           </div>
         </div>
