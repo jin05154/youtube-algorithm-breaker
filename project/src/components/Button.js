25d37218cfba007eddd9420a62ea0b2d17e4b8e8
Yumin <dorayuminlee@gmail.com>
2022-01-30 17:18:27 +0900

    [style] 페이지 구조 수정

--- project/src/components/Button.js
+++ project/src/components/Button.js
@@ -3,18 +3,16 @@ import styled from 'styled-components';
 
 export default function Button({ text, onClick }) {
     return (
-        <>
-            <StyledButton onClick={onClick}>
-                <span>{text}</span>
-            </StyledButton>
-        </>
+        <StyledButton onClick={onClick}>
+            <span>{text}</span>
+        </StyledButton>
     );
 }
 
 const theme = {
     pink: {
-      default: 'var(--button-default-color-1)',
-      hover: 'var(--button-hover-color-1)'
+        default: 'var(--button-default-color-1)',
+        hover: 'var(--button-hover-color-1)'
     }
 };
 
--- project/src/components/DefaultLayout.js
+++ project/src/components/DefaultLayout.js
@@ -1,26 +1,16 @@
-import styled from "styled-components";
-
 import Footer from "./Footer";
 import Header from "./Header";
 
 const DefaultLayout = ({ children }) => {
   return (
-    <>
-      <div className="App">
-        <Header />
-        <Body>{children}</Body>
-        <Footer />
+    <div className="App">
+      <Header />
+      <div className="default-body">
+        {children}
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
 export default DefaultLayout;
-
-const Body = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: calc(100% - 250px);
-  width: 100%;
-`;
