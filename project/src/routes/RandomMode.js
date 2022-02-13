e36298d0ec5dbf3d16aaffa3b0d98273709cdb03
Yumin <dorayuminlee@gmail.com>
2022-02-13 18:21:04 +0900

    [style] 로딩효과 spinner 추가

--- project/src/routes/RandomMode.js
+++ project/src/routes/RandomMode.js
@@ -2,24 +2,32 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import FadeIn from 'react-fade-in';
+import SyncLoader from 'react-spinners/SyncLoader';
 import Layout from "../components/layouts/Layout";
 import ItemContainer from '../components/ItemContainer';
 
 export default function RandomMode() {
+    const [loading, setLoading] = useState(true);
     const [videos, setVideos] = useState([]);
 
     const fetchVideoInfo = async () => {
-        axios.get('http://localhost:8080/api/video')
-            .then(res => setVideos(res.data))
-            .catch(err => console.log(err));
+        try {
+            const result = await axios.get('http://localhost:8080/api/video');
+            setVideos(result.data);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     useEffect(() => {
         fetchVideoInfo();
+        if (!videos) return <span>데이터 없음</span>
     }, []);
 
     return (
         <Layout type='recommend'>
+            {loading && <SyncLoader color='var(--black)' size={30} margin={5} />}
             {videos.map((v, i) =>
                 <FadeIn key={i}>
                     <ItemContainer url={`https://youtu.be/${v.url}`}>
