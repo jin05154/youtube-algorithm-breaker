b912bb092b9b3bbedc59ed776cd03bd49286ef71
Yumin <dorayuminlee@gmail.com>
2022-02-14 21:16:06 +0900

    [feat] youtube api에서 영상 duration 정보 가져오기 성공

--- project/backend/server/server.js
+++ project/backend/server/server.js
@@ -17,15 +17,19 @@ app.get('/api/video', (req, res) => {
         if (err) console.log(err);
         else {
             for (let i = 0; i < data.length; ++i) {
-                let res = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=${data[i].video_url}&key=${process.env.YT_API_KEY}`);
-                let sub = res.data.items[0].snippet
-                channels.push({ "id": i, "url": data[i].video_url, "video_title": sub.title, "channel_name": sub.channelTitle, "playtime": data[i].playtime });
+                let res = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=${data[i].video_url}&part=contentDetails&key=${process.env.YT_API_KEY}`);
+                const sub = res.data.items[0].snippet
+                const dur = res.data.items[0].contentDetails.duration
+                channels.push({ "id": i, "url": data[i].video_url, "video_title": sub.title, "channel_name": sub.channelTitle, "playtime": dur });
             }
             res.send(channels);
         }
     })
 })
 
+// TODO: parse duration string
+
+
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`)
+    console.log(`Listening on http://localhost:${PORT}/api/video`)
 })
