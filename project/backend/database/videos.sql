2a9b36a9e30b807a9db77d91def1a862da2c0b38
Yumin <dorayuminlee@gmail.com>
2022-02-23 16:11:14 +0900

    [chore] sql문 수정

--- project/backend/database/videos.sql
+++ project/backend/database/videos.sql
@@ -1,11 +1,7 @@
-CREATE TABLE videos (  
-    videoID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    video_title VARCHAR(255) NOT NULL,
-    video_url VARCHAR(30) NOT NULL,
-    channel_name VARCHAR(255) NOT NULL,
+CREATE TABLE videos (
+    video_url VARCHAR(30) NOT NULL PRIMARY KEY,
     first_category VARCHAR(30) NOT NULL,
     second_category VARCHAR(30),
-    playtime INT NOT NULL,
     age VARCHAR(10),
     sex INT,
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP
