0b4ff92c4060c07656d810675516d5d604e4477b
yumin <dorayuminlee@gmail.com>
2022-02-26 16:29:09 +0900

    [feat] '준비중' 알림 페이지 추가

--- project/src/App.js
+++ project/src/App.js
@@ -5,6 +5,7 @@ import "./app.css";
 import Main from "./routes/HomePage";
 import RandomMode from "./routes/RandomMode";
 import SelectMode from "./routes/SelectMode";
+import NotReadyPage from "./routes/NotReadyPage";
 import Admin from "./routes/AdminPage";
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="recommend" element={<SelectMode />} />
+      <Route path="recommend/survey" element={<NotReadyPage />} />
       <Route path="recommend/random" element={<RandomMode />} />
+      <Route path="faq" element={<NotReadyPage />} />
+      <Route path="learn-more" element={<NotReadyPage />} />
       <Route path="admin" element={<Admin />} />
     </Routes>
   );
---
+++ project/src/routes/NotReadyPage.js
@@ -0,0 +1,9 @@
+const NotReadyPage = () => {
+  return (
+    <div className="error-msg">
+      <h2>페이지 준비중입니다.</h2>
+    </div>
+  );
+};
+
+export default NotReadyPage;
