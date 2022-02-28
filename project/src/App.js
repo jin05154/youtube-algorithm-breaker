7e0513233e7d150c11c5d1bf02fecbee08ff8c54
yumin <dorayuminlee@gmail.com>
2022-02-28 16:13:06 +0900

    [chore] route path 변경

--- project/src/App.js
+++ project/src/App.js
@@ -11,13 +11,13 @@ import Admin from "./routes/AdminPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="recommend" element={<SelectMode />} />
-      <Route path="recommend/survey" element={<NotReadyPage />} />
-      <Route path="recommend/random" element={<RandomMode />} />
-      <Route path="faq" element={<NotReadyPage />} />
-      <Route path="learn-more" element={<NotReadyPage />} />
-      <Route path="admin" element={<Admin />} />
+      <Route path="/youtube-algorithm-breaker" element={<Main />} />
+      <Route path="/youtube-algorithm-breaker/recommend" element={<SelectMode />} />
+      <Route path="/youtube-algorithm-breaker/recommend/survey" element={<NotReadyPage />} />
+      <Route path="/youtube-algorithm-breaker/recommend/random" element={<RandomMode />} />
+      <Route path="/youtube-algorithm-breaker/faq" element={<NotReadyPage />} />
+      <Route path="/youtube-algorithm-breaker/learn-more" element={<NotReadyPage />} />
+      <Route path="/youtube-algorithm-breaker/admin" element={<Admin />} />
     </Routes>
   );
 }
--- project/src/components/layouts/Layout.js
+++ project/src/components/layouts/Layout.js
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 import Footer from "./Footer";
 import Header from "./Header";
 import { ReactComponent as Title } from "../../images/title.svg";
 
 export default function Layout({ children, type, mode }) {
+  const history = useNavigate();
+  const onClickLogo = () => history("/youtube-algorithm-breaker");
   return (
     <div className="App">
       {type === "default" && (
@@ -22,7 +26,9 @@ export default function Layout({ children, type, mode }) {
       {type === "onlyPC" && (
         <div className="pc-layout">
           <div className="pc-only-header">
-            <Title />
+            <div className="header-title">
+              <Title onClick={onClickLogo} />
+            </div>
             <h2>{mode}</h2>
           </div>
           <div className="pc-body">{children}</div>
