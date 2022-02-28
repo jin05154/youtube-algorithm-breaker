b9ac95f890c73ef45c95c03c2c0e245773945c50
yumin <dorayuminlee@gmail.com>
2022-02-28 16:20:12 +0900

    [fix] admin 페이지 logo 클릭되도록 수정

--- project/src/components/layouts/Layout.js
+++ project/src/components/layouts/Layout.js
@@ -1,12 +1,8 @@
-import { useNavigate } from "react-router-dom";
-
 import Footer from "./Footer";
 import Header from "./Header";
 import { ReactComponent as Title } from "../../images/title.svg";
 
 export default function Layout({ children, type, mode }) {
-  const history = useNavigate();
-  const onClickLogo = () => history("/youtube-algorithm-breaker");
   return (
     <div className="App">
       {type === "default" && (
@@ -26,9 +22,7 @@ export default function Layout({ children, type, mode }) {
       {type === "onlyPC" && (
         <div className="pc-layout">
           <div className="pc-only-header">
-            <div className="header-title">
-              <Title onClick={onClickLogo} />
-            </div>
+            <Title />
             <h2>{mode}</h2>
           </div>
           <div className="pc-body">{children}</div>
