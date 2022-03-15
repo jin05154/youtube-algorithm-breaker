e4be952040c52b6a1edb454d4f15cf0b9eabfc03
Yumin <dorayuminlee@gmail.com>
2022-03-16 01:04:47 +0900

    [chore] backend 삭제

--- project/backend/database/videos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE videos (
-    video_url VARCHAR(30) NOT NULL PRIMARY KEY,
-    first_category VARCHAR(30) NOT NULL,
-    second_category VARCHAR(30),
-    age VARCHAR(10),
-    sex INT,
-    update_time DATETIME DEFAULT CURRENT_TIMESTAMP
-);
