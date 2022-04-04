226217b2e5e7ea24b94034db4096aa1f2d402f85
Yumin <dorayuminlee@gmail.com>
2022-04-05 03:32:25 +0900

    [feat] admin 업로드 - 영상 url 클립보드에서 붙여넣기 기능 추가

--- project/src/components/utils/AdminUploadModal.js
+++ project/src/components/utils/AdminUploadModal.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 import styled, { keyframes } from "styled-components";
 import Button from "./Button";
 import { ReactComponent as CloseIcon } from "../../images/close.svg";
 
 export default function UploadModal({ closeUpload }) {
+  const [url, setUrl] = useState("");
+  const [pasteErr, setPasteErr] = useState(false);
+  // 복사한 영상 url 클립보드에서 가져오기
+  const getVideoUrl = () =>
+    setTimeout(async () => {
+      const text = await navigator.clipboard.readText();
+      if (text.substring(0, 17) === "https://youtu.be/") {
+        setPasteErr(false);
+        setUrl(text.substring(17));
+      } else setPasteErr(true);
+    });
+
   return (
     <Modal>
       <ModalHeader>
@@ -11,7 +25,44 @@ export default function UploadModal({ closeUpload }) {
           <CloseIcon />
         </CloseBtn>
       </ModalHeader>
-      <ModalBody>업로드 모달</ModalBody>
+      <ModalBody>
+        <BodyElement>
+          <BodyName>
+            <h4>영상 URL</h4>
+          </BodyName>
+          <p>{url}</p>
+          {url === "" && (
+            <Button
+              type="default"
+              theme="gray"
+              text="붙여넣기"
+              width="80px"
+              height="34px"
+              fontSize="15px"
+              margin="0"
+              onClick={getVideoUrl}
+            />
+          )}
+          {pasteErr && (
+            <ErrorMsg>영상의 주소가 제대로 복사되었는지 확인해주세요.</ErrorMsg>
+          )}
+        </BodyElement>
+        <BodyElement>
+          <BodyName>
+            <h4>카테고리</h4>
+          </BodyName>
+        </BodyElement>
+        <BodyElement>
+          <BodyName>
+            <h4>나이대</h4>
+          </BodyName>
+        </BodyElement>
+        <BodyElement>
+          <BodyName>
+            <h4>성별</h4>
+          </BodyName>
+        </BodyElement>
+      </ModalBody>
       <ModalFooter>
         <FooterWrapper>
           <Button
@@ -47,28 +98,28 @@ const modalAnimation = keyframes`
   }
 `;
 const Modal = styled.div`
-  width: 870px;
+  width: 800px;
   height: 500px;
   padding: 10px;
   background: var(--white);
   border: 1px solid var(--black);
   border-radius: 5px;
   z-index: 99;
-  box-shadow: rgba(0,0,0,0.6) 0 0 0 9999px;
+  box-shadow: rgba(0, 0, 0, 0.6) 0 0 0 9999px;
   position: fixed;
   top: 20vh;
   animation: ${modalAnimation} 0.3s;
 `;
 const ModalHeader = styled.div`
   height: 50px;
-  padding: 0 5px;
+  padding: 0 3px 0 14px;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 const ModalBody = styled.div`
   height: 390px;
-  padding: 5px;
+  padding: 5px 14px;
 `;
 const ModalFooter = styled.div`
   display: flex;
@@ -86,8 +137,26 @@ const CloseBtn = styled.button`
   background: none;
   cursor: pointer;
 `;
+const BodyElement = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const BodyName = styled.div`
+  display: flex;
+  align-items: center;
+  width: 80px;
+  height: 34px;
+  padding: 3px 0;
+`;
+const ErrorMsg = styled.p`
+  color: red;
+  font-size: 0.85em;
+  padding: 0 8px;
+`;
 const FooterWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   width: 186px;
+  padding: 0 3px;
 `;
